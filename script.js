0abbd41409e055c133fa05200c5af8bc85c85eeb
Adarsharc <arcadarsh1@gmail.com>
2022-12-12 22:54:51 +0530

    Created Dark Mode

--- script.js
+++ script.js
@@ -104,3 +104,17 @@ function timeSetUp(todo) {
   const date = new Date(todo.time);
   return date.toLocaleTimeString();
 }
+
+
+const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
+
+function switchTheme(e) {
+    if (e.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+    }
+    else {
+        document.documentElement.setAttribute('data-theme', 'light');
+    }    
+}
+
+toggleSwitch.addEventListener('change', switchTheme, false);
